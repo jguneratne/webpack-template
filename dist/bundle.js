d229abd4b0f940af1ba8de77bb85801d7ca6cda7
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-06-30 16:50:45 -0400

    First commit of template

---
+++ dist/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => {
+  // webpackBootstrap
+  var __webpack_exports__ = {};
+  /*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+  console.log("webpack setup");
+
+  /******/
+})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrLXRlbXBsYXRlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwid2VicGFjayBzZXR1cFwiKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
