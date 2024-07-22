c46d427b3f3fce77abd10665856e241364fd87a6
jguneratne <59374962+jguneratne@users.noreply.github.com>
2024-07-21 22:00:19 -0400

    Update configuration to use HTML Bundler Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,104 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlBundlerPlugin = require("html-bundler-webpack-plugin");
 
 module.exports = {
+  stats: { children: true },
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.join(__dirname, "dist/"),
     clean: true,
   },
-  devtool: "inline-source-map",
+
+  resolve: {
+    alias: {
+      "@scripts": path.join(__dirname, "src/js"),
+      "@styles": path.join(__dirname, "src/css"),
+      "@images": path.join(__dirname, "src/images"),
+    },
+  },
+
+  devtool: "source-map",
+
   plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack Template",
+    new HtmlBundlerPlugin({
+      entry: [
+        {
+          import: "./src/views/template.ejs", // template file
+          filename: "index.html", // => dist/index.html
+          data: {
+            title: "Webpack Template",
+          }, // pass variables into template
+        },
+      ],
+
+      js: {
+        // output filename for JS
+        filename: "js/[name].[contenthash:8].js",
+      },
+
+      css: {
+        // output filename for CSS
+        filename: "css/[name].[contenthash:8].css",
+      },
+
+      preprocessor: "ejs",
+
+      preprocessorOptions: {
+        async: false, // defaults 'false'
+        // defaults process.cwd(), root path for includes with an absolute path (e.g., /file.html)
+        root: path.join(__dirname, "src/views"), // defaults process.cwd()
+        // defaults [], an array of paths to use when resolving includes with relative paths
+        views: [
+          "src/partials", // relative path
+          path.join(__dirname, "src/partials"), // absolute path
+        ],
+      },
     }),
   ],
+
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.(css|sass|scss)$/,
+        use: [
+          {
+            loader: "css-loader",
+            options: {
+              import: false, // disable @import at-rules handling
+            },
+          },
+        ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|jpe?g|ico|svg)$/,
         type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        generator: {
+          filename: ({ filename }) => {
+            // Keep directory structure for images in dist folder
+            const srcPath = "src/assets/imgs";
+            const regExp = new RegExp(
+              `[\\\\/]?(?:${path.normalize(srcPath)}|node_modules)[\\\\/](.+?)$`
+            );
+            const assetPath = path.dirname(
+              regExp.exec(filename)[1].replace("@", "").replace(/\\/g, "/")
+            );
+
+            return `images/${assetPath}/[name].[hash:8][ext]`;
+          },
+        },
       },
     ],
+  },
+
+  // enable HMR with live reload
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    watchFiles: {
+      paths: ["src/**/*.*"],
+      options: {
+        usePolling: true,
+      },
+    },
   },
 };
